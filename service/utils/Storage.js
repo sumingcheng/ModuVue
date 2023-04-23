e1a292d938916ce4fe05ffeca18a2e4408751cc4
范晓豪 <fanxiaohaohaha@qq.com>
2023-04-23 11:26:09 +0800

    http

--- service/utils/Storage.js
+++ service/utils/Storage.js
@@ -1,21 +1,77 @@
-// 保存数据到本地存储
-export function setData(key, value) {
-  localStorage.setItem(key, JSON.stringify(value))
+const sessionCache = {
+  set(key, value) {
+    if (!sessionStorage) {
+      return
+    }
+    if (key != null && value != null) {
+      sessionStorage.setItem(key, value)
+    }
+  },
+  get(key) {
+    if (!sessionStorage) {
+      return null
+    }
+    if (key == null) {
+      return null
+    }
+    return sessionStorage.getItem(key)
+  },
+  setJSON(key, jsonValue) {
+    if (jsonValue != null) {
+      this.set(key, JSON.stringify(jsonValue))
+    }
+  },
+  getJSON(key) {
+    const value = this.get(key)
+    if (value != null) {
+      return JSON.parse(value)
+    }
+  },
+  remove(key) {
+    sessionStorage.removeItem(key)
+  }
 }
 
-// 从本地存储中获取数据
-export function getData(key) {
-  const value = localStorage.getItem(key)
-  // JSON.parse() 方法解析 JSON 字符串并返回 JavaScript 值
-  return value ? JSON.parse(value) : null
+const localCache = {
+  set(key, value) {
+    if (!localStorage) {
+      return
+    }
+    if (key != null && value != null) {
+      localStorage.setItem(key, value)
+    }
+  },
+  get(key) {
+    if (!localStorage) {
+      return null
+    }
+    if (key == null) {
+      return null
+    }
+    return localStorage.getItem(key)
+  },
+  setJSON(key, jsonValue) {
+    if (jsonValue != null) {
+      this.set(key, JSON.stringify(jsonValue))
+    }
+  },
+  getJSON(key) {
+    const value = this.get(key)
+    if (value != null) {
+      return JSON.parse(value)
+    }
+  },
+  remove(key) {
+    localStorage.removeItem(key)
+  }
 }
 
-// 从本地存储中删除数据
-export function removeData(key) {
-  localStorage.removeItem(key)
-}
-
-// 清除本地存储中的所有数据
-export function clearAllData() {
-  localStorage.clear()
+export default {
+  /**
+   * 会话级缓存
+   */
+  session: sessionCache,
+  /**
+   * 本地缓存
+   */ local: localCache
 }
