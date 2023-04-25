d3f63a43f646d99cfd99029a9a951be53be9da89
范晓豪 <fanxiaohaohaha@qq.com>
2023-04-25 09:40:39 +0800

    prod环境

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import '@/assets/less/index.less'
 import '@/assets/css/reset.css'
 
-// const isProd = process.env.NODE_ENV === 'production'
-
+const isProd = process.env.NODE_ENV
+console.log('isProd', isProd)
 const app = createApp(App)
 app.mount('#app')
---
+++ src/views/login/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('test')
+</script>
+
+<template>
+  <div>{{ message }}</div>
+</template>
+
+<style scoped lang="less">
+div {
+  font-size: 25px;
+  color: #333;
+  text-align: center;
+}
+</style>
