07dfef23b450fe4c667e87edcd3c2fc975cf114f
范晓豪 <fanxiaohaohaha@qq.com>
2023-05-06 10:50:55 +0800

    new

---
+++ src/layout/base/service-config/store.js
@@ -0,0 +1,15 @@
+export default {
+  namespaced: true,
+  state: {
+    // 应用程序的状态
+  },
+  mutations: {
+    // 更新应用程序状态的方法
+  },
+  actions: {
+    // 触发应用程序 mutations 的方法
+  },
+  getters: {
+    // 计算 state 属性的方法
+  }
+}
