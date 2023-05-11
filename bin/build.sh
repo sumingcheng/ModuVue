1626005d4b7a1c611b94e336540538e135788652
范晓豪 <fanxiaohaohaha@qq.com>
2023-05-11 16:33:12 +0800

    fix：shell

--- bin/build.sh
+++ bin/build.sh
@@ -1,16 +1,40 @@
 bin/build_icon.sh
 
 # 增加版本号
-npm version patch
-#npm version minor
-#npm version major
 
-# 项目打包
-webpack --config config/webpack.build.js
+echo "是否增加版本号: "
+options=("Yes" "No" "Quit")
+select opt in "${options[@]}"
 
-# 移动
-mv dist/resource/stats.json Analyzer/
-mv dist/resource/report.html Analyzer/
+do
+    case $opt in
+        "Yes")
+            npm version patch
+            build
+            ;;
+        "No")
+            echo "不增加版本号"
+            build
+            ;;
+        "Quit")
+            echo "以退出"
+            break
+            ;;
+        *) # 用户输入无效选项时的处理
+            echo "Invalid option, please try again"
+            ;;
+    esac
+done
 
-# zip
-npm run zip
+build(){
+  # 项目打包
+  webpack --config config/webpack.build.js
+
+  # 移动
+  mv dist/resource/stats.json Analyzer/
+  mv dist/resource/report.html Analyzer/
+
+  # zip
+  npm run zip
+}
+
